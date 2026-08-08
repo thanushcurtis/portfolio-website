8ea3612164507e9c838370b5ea2edb060654b6ca
Thanush Curtis <thanushcurtis@gmail.com>
2026-08-08 22:31:33 +0100

    Update Google Analytics measurement ID to G-M32WSJXLFW

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
-      <GoogleAnalytics gaId="G-6GXVMMYMBZ" />
+      <GoogleAnalytics gaId="G-M32WSJXLFW" />
     </html>
   );
 }
